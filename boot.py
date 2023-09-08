c1e2aa538225d6e64d81b0a0258939e0da9e442f
HumanBojack <spychalar59@gmail.com>
2023-09-08 17:42:36 +0200

    refactor: comment and simplify boot.py

--- boot.py
+++ boot.py
@@ -2,8 +2,13 @@ from board import *
 import digitalio
 import storage
 
-# Only activate the usb_drive if the GP0 pin is connected
-progStatusPin = digitalio.DigitalInOut(GP0)
-progStatusPin.switch_to_input(pull=digitalio.Pull.UP)
-if progStatusPin.value:
+# Define the pin to check for USB drive activation
+usb_activation_pin = digitalio.DigitalInOut(GP0)
+
+# Set the pin to input mode with pull-up resistor enabled
+usb_activation_pin.switch_to_input(pull=digitalio.Pull.UP)
+
+# Check if the pin is NOT connected to ground (i.e. USB drive is not activated)
+if usb_activation_pin.value:
+    # Disable the USB drive
     storage.disable_usb_drive()
